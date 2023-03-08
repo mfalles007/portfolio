1c36a21eaf6430379385faf596a97f0d065ad52a
mfalles007 <mfalles007@gmail.com>
2023-03-08 14:57:21 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+
+
